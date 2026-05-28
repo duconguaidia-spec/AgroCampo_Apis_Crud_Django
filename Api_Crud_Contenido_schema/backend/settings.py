c3034c3804f5d054a566176d94c5e59828116564
Katha Gualdrón <gualdronmarquezw2008@gmail.com>
2026-05-28 17:04:23 -0500

    Se empieca a modificar la informacion del backeng archivo settings

--- Api_Crud_Contenido_schema/backend/settings.py
+++ Api_Crud_Contenido_schema/backend/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
-from pathlib import Path
+from pathlib import Path 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #APP para usar el APIA REST
+    'rest_framework',
+    #'APP para usar el swagger'
+    'drf_yasg',
+    #App del api
+    'api',
 ]
 
 MIDDLEWARE = [
