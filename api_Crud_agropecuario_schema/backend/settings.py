301775a2dc8901b83bd3b2e4428ff1eee8a4b3ea
mauricioponguta <pongutaerin18@gmail.com>
2026-05-28 17:04:55 -0500

    Configurar rest framework, drf_yasg y app api

--- api_Crud_agropecuario_schema/backend/settings.py
+++ api_Crud_agropecuario_schema/backend/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'rest_framework',
+    
+    'drf_yasg',
+    
+    'api',
+    
 ]
 
 MIDDLEWARE = [
