4b171128c650462145bad78517b53071db303649
Katha Gualdrón <gualdronmarquezw2008@gmail.com>
2026-05-30 11:44:04 -0500

    Se siguen haciendo cambios en las migrations ,y en los backend

--- Api_Crud_Contenido_schema/api/models.py
+++ Api_Crud_Contenido_schema/api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+#Modelo tabla categoria
+class Categoria(models.Model):
+    id_categoria = models.AutoField(primary_key=True)
+    nombre_categoria = models.CharField(max_length=80)
+    activo = models.BooleanField(default=True)
+    fecha_creacion = models.DateTimeField(default=timezone.now)
+    fecha_modificacion = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.nombre_categoria
+
+    class Meta:
+        db_table = 'categoria'
+
+    )
--- Api_Crud_Contenido_schema/backend/settings.py
+++ Api_Crud_Contenido_schema/backend/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path 
+from decouple import config
 
+DEBUG=config('DEBUG', cast=bool)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -81,7 +83,14 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
+        'OPTIONS': {
+            'options': f'-c search_path={config("DB_SCHREMA")}'
+        }
     }
 }
 
