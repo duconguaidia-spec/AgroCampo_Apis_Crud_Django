00db64a38ac83f653a3d83568aa2c99713312dd7
mauricioponguta <pongutaerin18@gmail.com>
2026-06-01 15:03:12 -0500

    feat(views): crear viewsets para operaciones CRUD

--- api_Crud_agropecuario_schema/api/views.py
+++ api_Crud_agropecuario_schema/api/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
+from rest_framework import viewsets
+
+from .models import (
+    CategoriaGanado,
+    Subasta,
+    TrPreciosubastaganado
+)
+from .serializers import (
+    CategoriaGanadoSerializer,
+    SubastaSerializer,
+    TrPreciosubastaganadoSerializer
+)
 # Create your views here.
+
+class CategoriaGanadoViewSet(viewsets.ModelViewSet):
+    queryset = CategoriaGanado.objects.all()
+    serializer_class = CategoriaGanadoSerializer
+
+class SubastaViewSet(viewsets.ModelViewSet):
+    queryset = Subasta.objects.all()
+    serializer_class = SubastaSerializer
+    
+class TrPreciosubastaganadoViewSet(viewsets.ModelViewSet):
+    queryset = TrPreciosubastaganado.objects.all()
+    serializer_class = TrPreciosubastaganadoSerializer
+
