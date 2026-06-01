2d7d6638a70624132fecb18239f13d96ab0c48b9
duconguaidia-spec <duconguaidia@gmail.com>
2026-06-01 14:30:52 -0500

    Se agrega viewset de noticias con operaciones CRUD

--- Api_Crud_Noticias_Schema/api/views.py
+++ Api_Crud_Noticias_Schema/api/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+
+from .models import (
+    Noticias
+)
+
+from .serializers import (
+    NoticiasSerializer
+)
+
+# Crud de tabla jugador (GET, POST, PUT, DELETE)
+
+class NoticiasViewSet(viewsets.ModelViewSet):
+    queryset = Noticias.objects.all()
+    serializer_class = NoticiasSerializer
