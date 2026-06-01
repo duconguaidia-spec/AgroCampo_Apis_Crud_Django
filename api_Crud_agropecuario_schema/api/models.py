db58eb5e0203546cacb4218440180f3fa3a813c0
mauricioponguta <pongutaerin18@gmail.com>
2026-06-01 14:48:14 -0500

    feat(models): generar y configurar modelos del esquema agropecuario

--- api_Crud_agropecuario_schema/api/models.py
+++ api_Crud_agropecuario_schema/api/models.py
@@ -7,8 +7,10 @@ class CategoriaGanado(models.Model):
     codigo = models.CharField(unique=True, max_length=10)
     descripcion = models.CharField(max_length=100)
     activo = models.BooleanField()
-    fecha_creacion = models.DateTimeField()
-    fecha_modificacion = models.DateTimeField()
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_modificacion = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.descripcion
 
     class Meta:
         managed = False
@@ -22,9 +24,10 @@ class Subasta(models.Model):
     precio = models.DecimalField(max_digits=10, decimal_places=2)
     id_usuario_registra = models.IntegerField()
     activo = models.BooleanField()
-    fecha_creacion = models.DateTimeField()
-    fecha_modificacion = models.DateTimeField()
-
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_modificacion = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.ubicacion
     class Meta:
         managed = False
         db_table = 'subasta'
@@ -35,9 +38,10 @@ class TrPreciosubastaganado(models.Model):
     id_subasta = models.ForeignKey(Subasta, models.DO_NOTHING, db_column='id_subasta')
     id_categoria_ganado = models.ForeignKey(CategoriaGanado, models.DO_NOTHING, db_column='id_categoria_ganado')
     activo = models.BooleanField()
-    fecha_creacion = models.DateTimeField()
-    fecha_modificacion = models.DateTimeField()
-
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_modificacion = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return f"TrPreciosubastagando {self}"
     class Meta:
         managed = False
         db_table = 'tr_precioSubastaGanado'
