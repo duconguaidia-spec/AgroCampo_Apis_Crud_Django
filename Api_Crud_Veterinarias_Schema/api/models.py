f2ec0206aa01317b3da0bdec933b9a77412f0885
duconguaidia-spec <duconguaidia@gmail.com>
2026-06-01 22:03:39 -0500

    se reorganizaron las tablas de manera que quede primero las que no necesitan la informacion de las otras y de ultima las otras

--- Api_Crud_Veterinarias_Schema/api/models.py
+++ Api_Crud_Veterinarias_Schema/api/models.py
@@ -54,6 +54,72 @@ class Veterinaria(models.Model):
         db_table = 'Veterinaria'
 
 
+# Modelo tabla ProductoVeterinaria
+class ProductoVeterinaria(models.Model):
+    id_producto_vet = models.AutoField(primary_key=True)
+    id_veterinaria = models.ForeignKey(
+        Veterinaria,
+        on_delete=models.CASCADE,
+        db_column='id_veterinaria'
+    )
+    nombre_producto = models.CharField(max_length=100)
+    descripcion = models.CharField(max_length=255, null=True, blank=True)
+    precio = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    activo = models.BooleanField(default=True)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_modificacion = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.nombre_producto
+
+    class Meta:
+        db_table = 'ProductoVeterinaria'
+
+
+# Modelo tabla ResenaVeterinaria
+class ResenaVeterinaria(models.Model):
+    id_resena = models.AutoField(primary_key=True)
+    id_veterinaria = models.ForeignKey(
+        Veterinaria,
+        on_delete=models.CASCADE,
+        db_column='id_veterinaria'
+    )
+    id_usuario = models.IntegerField()
+    calificacion = models.IntegerField()
+    comentario = models.CharField(max_length=500, null=True, blank=True)
+    activo = models.BooleanField(default=True)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_modificacion = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'Reseña {self.id_resena} - {self.id_veterinaria}'
+
+    class Meta:
+        db_table = 'ResenaVeterinaria'
+
+
+# Modelo tabla ServicioVeterinaria
+class ServicioVeterinaria(models.Model):
+    id_servicio = models.AutoField(primary_key=True)
+    id_veterinaria = models.ForeignKey(
+        Veterinaria,
+        on_delete=models.CASCADE,
+        db_column='id_veterinaria'
+    )
+    nombre_servicio = models.CharField(max_length=100)
+    descripcion = models.CharField(max_length=255, null=True, blank=True)
+    precio = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    activo = models.BooleanField(default=True)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_modificacion = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.nombre_servicio
+
+    class Meta:
+        db_table = 'ServicioVeterinaria'
+
+
 # Modelo tabla tr_VeterinariaEspecialidad
 class VeterinariaEspecialidad(models.Model):
     id_vet_especialidad = models.AutoField(primary_key=True)
@@ -102,69 +168,3 @@ class VeterinariaServicio(models.Model):
     class Meta:
         db_table = 'tr_VeterinariaServicio'
         unique_together = ('id_veterinaria', 'id_servicio_general')
-
-
-# Modelo tabla ServicioVeterinaria
-class ServicioVeterinaria(models.Model):
-    id_servicio = models.AutoField(primary_key=True)
-    id_veterinaria = models.ForeignKey(
-        Veterinaria,
-        on_delete=models.CASCADE,
-        db_column='id_veterinaria'
-    )
-    nombre_servicio = models.CharField(max_length=100)
-    descripcion = models.CharField(max_length=255, null=True, blank=True)
-    precio = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
-    activo = models.BooleanField(default=True)
-    fecha_creacion = models.DateTimeField(auto_now_add=True)
-    fecha_modificacion = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.nombre_servicio
-
-    class Meta:
-        db_table = 'ServicioVeterinaria'
-
-
-# Modelo tabla ProductoVeterinaria
-class ProductoVeterinaria(models.Model):
-    id_producto_vet = models.AutoField(primary_key=True)
-    id_veterinaria = models.ForeignKey(
-        Veterinaria,
-        on_delete=models.CASCADE,
-        db_column='id_veterinaria'
-    )
-    nombre_producto = models.CharField(max_length=100)
-    descripcion = models.CharField(max_length=255, null=True, blank=True)
-    precio = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
-    activo = models.BooleanField(default=True)
-    fecha_creacion = models.DateTimeField(auto_now_add=True)
-    fecha_modificacion = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.nombre_producto
-
-    class Meta:
-        db_table = 'ProductoVeterinaria'
-
-
-# Modelo tabla ResenaVeterinaria
-class ResenaVeterinaria(models.Model):
-    id_resena = models.AutoField(primary_key=True)
-    id_veterinaria = models.ForeignKey(
-        Veterinaria,
-        on_delete=models.CASCADE,
-        db_column='id_veterinaria'
-    )
-    id_usuario = models.IntegerField()
-    calificacion = models.IntegerField()
-    comentario = models.CharField(max_length=500, null=True, blank=True)
-    activo = models.BooleanField(default=True)
-    fecha_creacion = models.DateTimeField(auto_now_add=True)
-    fecha_modificacion = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return f'Reseña {self.id_resena} - {self.id_veterinaria}'
-
-    class Meta:
-        db_table = 'ResenaVeterinaria'
