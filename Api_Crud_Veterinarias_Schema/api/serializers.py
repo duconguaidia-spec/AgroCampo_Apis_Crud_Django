4cb645db8c469bbb883e4e89e580153a897dad73
duconguaidia-spec <duconguaidia@gmail.com>
2026-06-01 21:43:14 -0500

    Se crea serializador del modelo Veterinarias para convertir los datos a JSON

---
+++ Api_Crud_Veterinarias_Schema/api/serializers.py
@@ -0,0 +1,68 @@
+from rest_framework import serializers
+
+from .models import (
+    Especialidad,
+    ServicioGeneral,
+    Veterinaria,
+    VeterinariaEspecialidad,
+    VeterinariaServicio,
+    ServicioVeterinaria,
+    ProductoVeterinaria,
+    ResenaVeterinaria,
+)
+
+
+# Serializador para Especialidad
+class EspecialidadSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Especialidad
+        fields = '__all__'
+
+
+# Serializador para ServicioGeneral
+class ServicioGeneralSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ServicioGeneral
+        fields = '__all__'
+
+
+# Serializador para Veterinaria
+class VeterinariaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Veterinaria
+        fields = '__all__'
+
+
+# Serializador para VeterinariaEspecialidad
+class VeterinariaEspecialidadSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = VeterinariaEspecialidad
+        fields = '__all__'
+
+
+# Serializador para VeterinariaServicio
+class VeterinariaServicioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = VeterinariaServicio
+        fields = '__all__'
+
+
+# Serializador para ServicioVeterinaria
+class ServicioVeterinariaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ServicioVeterinaria
+        fields = '__all__'
+
+
+# Serializador para ProductoVeterinaria
+class ProductoVeterinariaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductoVeterinaria
+        fields = '__all__'
+
+
+# Serializador para ResenaVeterinaria
+class ResenaVeterinariaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ResenaVeterinaria
+        fields = '__all__'
